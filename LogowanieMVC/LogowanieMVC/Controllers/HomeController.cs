df50fa4504d56c7d277f71ff545ce020098bd5a1
gawarr <67899290+gawarr@users.noreply.github.com>
2020-07-10 13:42:44 +0200

    first commit

---
+++ LogowanieMVC/LogowanieMVC/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using LogowanieMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LogowanieMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            LogIn.EnterData();
+            ViewBag.Login = LogIn.login;
+
+            return View();
+        
+        }
+    }
+}
---
+++ LogowanieMVC/LogowanieMVC/Models/LogIn.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace LogowanieMVC.Models
+{
+    public class LogIn
+    {
+        public static string login;
+        public static void EnterData()
+        {
+            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Praktyki\\source\\repos\\LogowanieMVC\\LogowanieMVC\\App_Data\\BAZA.mdf;Integrated Security=True";
+            string query = "SELECT Login,  From dbo.UsersLoginData WHERE Id = @id";
+            int id = 1;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("id", id);
+
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                reader.Read();
+
+                login = (string)reader[0];
+            }
+        }
+        public static void Check()
+        {
+            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Praktyki\\source\\repos\\LogowanieMVC\\LogowanieMVC\\App_Data\\BAZA.mdf;Integrated Security=True";
+            string query = "SELECT Login,  From dbo.UsersLoginData WHERE Id = @id";
+            int id = 1;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("id", id);
+
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                reader.Read();
+
+                login = (string)reader[0];
+            }
+        }
+    }
+}
